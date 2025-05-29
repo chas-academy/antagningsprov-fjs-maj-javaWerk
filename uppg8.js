78e5c14e815c1476336c179a04f5ce461071234a
javaWerk <johan@transformwerk.tech>
2025-05-29 08:34:40 +0200

    uppg8 Done

--- uppg8.js
+++ uppg8.js
@@ -2,16 +2,30 @@
 
 function uppg8(){
 
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
+    // skapa en array med fem person obkjet
+    // varje person objekt ska ha två properties, name och age
 
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
+    // skapa en fuktion som tar in en array som argument
+    // skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
+    //  över 30 år som skrivs ut i konsolen
 
-// anropa funktionen och skicka med arrayen som argument
+    // anropa funktionen och skicka med arrayen som argument
 
+    let names = [{name:"Johan", age:30}, {name:"Annette", age:45}, {name:"Peter", age:42}, {name:"Sofie", age:13}, {name:"Kalle", age:54}];
+
+    function overThirty(personArray){
+
+        for (person of personArray){
+            if (person.age > 30){
+                console.log(person.name);
+            }
+        }
+    }
+
+    overThirty(names);
 
 }
+
+
 
 module.exports = { uppg8 };
