d609beec950bf2c8507b424112a8130f424bba5f
javaWerk <johan@transformwerk.tech>
2025-05-29 08:20:35 +0200

    uppg6 Done

--- uppg6.js
+++ uppg6.js
@@ -4,8 +4,12 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+    let names = ["Johan", "Annette", "Peter", "Sofie", "Kalle", "Hobbe"]
 
+    for (name in names){
+        console.log(names[name])
+    }
  
 }
-
+uppg6()
 module.exports = { uppg6 };
