77600bca0cc428e795b8e0550e8654e96ee442a0
javaWerk <johan@transformwerk.tech>
2025-05-29 08:55:58 +0200

    uppg9 Done

--- uppg9.js
+++ uppg9.js
@@ -7,8 +7,24 @@ function uppg9 (){
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
 
     // anropa funktionen och skicka med en array som argument
+    numbers =[6,3,7,1,2,4,6,88,34,23,0,101,44,67,76];
+
+    function findOddEven(arrayOfNumbers){
+
+        for (number of arrayOfNumbers){
+            if (number % 2 == 0) {
+                console.log(number + " jämt");
+            } else {
+                console.log(number + "  udda");
+            }
+        }
+    }
+
+    findOddEven(numbers);
 
   
 }
+
+uppg9 ();
 
 module.exports = { uppg9 };
